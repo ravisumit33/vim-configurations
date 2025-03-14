78bc40ad3ac1afcc73b725bed56ef7083b93207d
ravisumit33 <ravi.sumit33@gmail.com>
2025-03-15 00:05:19 +0530

    Add avante plugin

--- lazy/lua/plugins/blink.lua
+++ lazy/lua/plugins/blink.lua
@@ -1,9 +1,28 @@
 return {
   {
     "saghen/blink.cmp",
+    dependencies = {
+      "Kaiser-Yang/blink-cmp-avante",
+    },
     opts = function(_, opts)
+      opts.keymap = opts.keymap or {}
       opts.keymap["<CR>"] = {} -- Remove the original <CR> mapping
       opts.keymap["<S-CR>"] = { "accept", "fallback" }
+
+      opts.sources = opts.sources or {}
+      opts.sources.default = opts.sources.default or {}
+      -- Extend the default list to include 'avante' if not already present
+      if not vim.tbl_contains(opts.sources.default, "avante") then
+        table.insert(opts.sources.default, "avante")
+      end
+      opts.sources.providers = opts.sources.providers or {}
+      opts.sources.providers.avante = {
+        module = "blink-cmp-avante",
+        name = "Avante",
+        opts = {
+          -- options for blink-cmp-avante
+        },
+      }
       return opts
     end,
   },
--- lazy/lua/plugins/nvim-autopairs.lua
+++ lazy/lua/plugins/nvim-autopairs.lua
@@ -2,7 +2,9 @@ return {
   {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
-    opts = {},
+    opts = {
+      fast_wrap = {},
+    },
   },
   {
     "echasnovski/mini.pairs",
